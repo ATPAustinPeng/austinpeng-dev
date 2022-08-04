97ab63fb7582d24c620359f6142d3b7487de80e5
ATPAustinPeng <austinpeng2002@gmail.com>
2022-08-04 00:40:03 -0700

    ATP-27: add feed component & base page view for  page

---
+++ frontend/components/feed.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Feed(props) {
+	console.log(props);
+	return (
+		<>
+			{props.posts.map((post, index) => {
+				return (
+					<div key={post._id}>
+						<Link href={`/writing/post${index}`}>
+							<a>
+								<h1>{post.title}</h1>
+							</a>
+						</Link>
+						<h2>{post.description}</h2>
+						<h4>{post.content}</h4>
+						<h4>{post.tags}</h4>
+						<h4>{post.lengthOfRead}</h4>
+						<h4>{post.date}</h4>
+					</div>
+				);
+			})}
+		</>
+	);
+}
---
+++ frontend/pages/writing.js
@@ -0,0 +1,15 @@
+import Feed from "../components/feed";
+
+export default function Writing({ posts }) {
+	return <Feed posts={posts.posts} />;
+}
+
+export async function getServerSideProps() {
+	// Fetch data from external API
+	const host = process.env.HOST || "http://localhost:3000";
+	const res = await fetch(host + "/api/posts");
+	const posts = await res.json();
+
+	// Pass data to the page via props
+	return { props: { posts } };
+}
