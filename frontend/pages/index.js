85932ac32397150223f7e74fe5894b9ddc6db393
ATPAustinPeng <austinpeng2002@gmail.com>
2022-08-04 13:35:03 -0700

    ATP-27: remove unnecessary references to MongoDB and getServerSideProps() from Home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,9 +2,9 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home({ posts }) {
-	if (posts.notFound == true) {
-		return (
+export default function Home() {
+	return (
+		<>
 			<Layout home>
 				<Head>
 					<title>{siteTitle}</title>
@@ -17,43 +17,6 @@ export default function Home({ posts }) {
 					</p>
 				</section>
 			</Layout>
-		);
-	} else {
-		return (
-			<>
-				<Layout home>
-					<Head>
-						<title>{siteTitle}</title>
-					</Head>
-					<section className={utilStyles.headingMd}>
-						<p>Hello, and welcome to my personal website!</p>
-						<p>
-							I will be updating this website periodically with
-							new features and eventually blog posts!
-						</p>
-					</section>
-				</Layout>
-				{/*<ul>*/}
-				{/*	{posts.posts.map((post) => {*/}
-				{/*		return (*/}
-				{/*			<div key={post._id}>*/}
-				{/*				<h1>{post.title}</h1>*/}
-				{/*				<h3>{post.description}</h3>*/}
-				{/*			</div>*/}
-				{/*		);*/}
-				{/*	})}*/}
-				{/*</ul>*/}
-			</>
-		);
-	}
+		</>
+	);
 }
-
-// export async function getServerSideProps() {
-// 	// Fetch data from external API
-// 	const host = process.env.HOST || "http://localhost:3000";
-// 	const res = await fetch(host + "/api/posts");
-// 	const posts = await res.json();
-//
-// 	// Pass data to the page via props
-// 	return { props: { posts } };
-// }
